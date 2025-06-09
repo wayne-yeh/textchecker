87292a294368e60796c40e2cdf16df9575bbd129
Wayne Yeh <thebest77771023@yahoo.com.tw>
2025-06-09 10:44:21 +0800

    add
    test

---
+++ src/test/java/com/example/control_f/ControlFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control_f;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
